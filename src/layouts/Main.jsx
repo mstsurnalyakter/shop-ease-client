58d04abbb2e94310d4d84c9205a68a4f30ad00b5
mstsurnalyakter <surnalyms@gmail.com>
2024-08-15 10:56:47 +0600

    add: Main layout

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+const Main = () => {
+  return (
+    <div className="font-poppins dark:bg-blue-gray-900  duration-100">
+      {/* <NavBar /> */}
+
+      {/* Main Content */}
+      <div className="mx-auto max-w-7xl px-7 lg:px-2 mt-10 mb-10 min-h-[calc(100vh-432px)]">
+        <Outlet />
+      </div>
+      {/* <Footer /> */}
+      <Toaster />
+    </div>
+  );
+};
+
+export default Main;
