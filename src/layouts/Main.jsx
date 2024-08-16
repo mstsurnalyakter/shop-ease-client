41e4f46a7ecceb3ec2ca13fededc04a1ac811600
mstsurnalyakter <surnalyms@gmail.com>
2024-08-16 23:27:53 +0600

    add: Footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import NavBar from "../pages/NavBar";
+import Footer from "../pages/shared/Footer";
 const Main = () => {
   return (
     <div className="font-poppins dark:bg-blue-gray-900  duration-100">
@@ -10,7 +11,7 @@ const Main = () => {
       <div className="mx-auto max-w-7xl px-7 lg:px-2 mt-10 mb-10 min-h-[calc(100vh-432px)]">
         <Outlet />
       </div>
-      {/* <Footer /> */}
+      <Footer />
       <Toaster />
     </div>
   );
